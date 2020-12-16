8d174c961975f97413e2d54db7545dcaf87e1f20
abhisek <abhiseksahoo33@gmail.com>
2020-12-16 11:50:07 +0530

    added properties file to Example11

--- src/main/java/org/example/spring/Example11/config/Appconfig.java
+++ src/main/java/org/example/spring/Example11/config/Appconfig.java
@@ -3,7 +3,7 @@ package org.example.spring.Example11.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-//@PropertySource("person data")
+@Configuration// this is reuired to resolve $ symbols in @Value annotations, default values injected.
+@PropertySource("person-data.properties")
 public class Appconfig {
 }
